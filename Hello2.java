8e5993a98e27d279989fad8fa9dbb343f2ab66f2
min gyun kim <60118539+Dasari03@users.noreply.github.com>
2022-10-27 22:53:58 -0700

    Add files via upload
    Learning how to print out strings literal and assign variables.

---
+++ Hello2.java
@@ -0,0 +1,15 @@
+public class Hello2 {
+    public static void main(String[] args){
+        System.out.println("Hello MGK");
+
+        int myFirstNumber = 7;
+        int mySecondNumber = 11;
+        int myThirdNumber = 23;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+
+        System.out.println(myFirstNumber);
+        System.out.println(myFirstNumber + mySecondNumber + myThirdNumber);
+        System.out.println(myLastOne);
+    }
+}
